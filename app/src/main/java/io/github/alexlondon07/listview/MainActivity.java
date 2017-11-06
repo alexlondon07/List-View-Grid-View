c42eb962719922a6689c2a93df247bcdef5600e0
Alexander Londoño Espejo <alexlondon07@gmail.com>
2017-11-05 20:00:13 -0500

    Listview Click

--- app/src/main/java/io/github/alexlondon07/listview/MainActivity.java
+++ app/src/main/java/io/github/alexlondon07/listview/MainActivity.java
@@ -1,9 +1,12 @@
 package io.github.alexlondon07.listview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +14,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
 
     private ListView listView;
+    private List<String> names;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,16 +24,29 @@ public class MainActivity extends AppCompatActivity {
         listView = findViewById(R.id.listView_item);
 
         //Datos a mostrar
-        List<String> names = new ArrayList<>();
+        names = new ArrayList<>();
         names.add("Alexander Londoño");
         names.add("Emiliana Londoño");
         names.add("Estefany Martinez");
         names.add("Alejandro Londoño");
+        names.add("Aracelly Espejo");
+        names.add("Jesus Londoño");
+        names.add("Daniela Martinez");
+        names.add("Isabel");
 
         //Adaptador, forma visual en que mostramos nuestros datos
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, names);
 
         //Establecemos el adaptardor con nuestros Listview
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Clicked " +names.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
