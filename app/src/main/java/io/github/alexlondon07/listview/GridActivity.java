8b329d9c394fe6c684e4236d7068fa0df3dae84c
Alexander Londoño Espejo <alexlondon07@gmail.com>
2017-11-17 22:30:08 -0500

    Context Menu

--- app/src/main/java/io/github/alexlondon07/listview/GridActivity.java
+++ app/src/main/java/io/github/alexlondon07/listview/GridActivity.java
@@ -2,6 +2,7 @@ package io.github.alexlondon07.listview;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -56,9 +57,12 @@ public class GridActivity extends AppCompatActivity {
         myadapter = new Myadapter(this, R.layout.grid_item, names);
         gridView.setAdapter(myadapter);
 
+        registerForContextMenu(gridView);
+
     }
 
 
+    //Inflamos el layout del menu de opciones
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
@@ -66,7 +70,7 @@ public class GridActivity extends AppCompatActivity {
         return true;
     }
 
-
+    //Manejamos evento click en el menu opciones
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
@@ -82,5 +86,37 @@ public class GridActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
         }
 
+    }
+
+    //Inflamos el layout del context menu
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+        menu.setHeaderTitle(names.get(info.position));
+        inflater.inflate(R.menu.context_menu, menu);
+    }
+
+
+    //Manejamos evento click en el context menu
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)item.getMenuInfo() ;
+
+        switch (item.getItemId()){
+
+            case R.id.delete_item:
+                //Borramos item clickeado
+                this.names.remove(info.position);
+                //Notificamos al adaptador que se actualice
+                this.myadapter.notifyDataSetChanged();
+                return true;
+
+            default:
+                return super.onContextItemSelected(item);
+        }
     }
 }
